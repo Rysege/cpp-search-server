b64d876dabce78ac2788ecd5acb4422889ab89e1
Sergey <s-vishnewsky@yandex.ru>
2024-03-16 17:48:24 +0300

    Добавлен проект Поисковый сервер

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -1,4 +1,191 @@
 // Решите загадку: Сколько чисел от 1 до 1000 содержат как минимум одну цифру 3?
-// Напишите ответ здесь:
+// Напишите ответ здесь: 271
 
 // Закомитьте изменения и отправьте их в свой репозиторий.
+
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <set>
+#include <cmath>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+const int MAX_RESULT_DOCUMENT_COUNT = 5;
+
+string ReadLine() {
+    string s;
+    getline(cin, s);
+    return s;
+}
+
+int ReadLineWithNumber() {
+    int result = 0;
+    cin >> result;
+    ReadLine();
+    return result;
+}
+
+vector<string> SplitIntoWords(const string& text) {
+    vector<string> words;
+    string word;
+
+    for (const char c : text) {
+        if (c == ' ') {
+            if (!word.empty()) {
+                words.push_back(word);
+                word.clear();
+            }
+        }
+        else {
+            word += c;
+        }
+    }
+
+    if (!word.empty()) {
+        words.push_back(word);
+    }
+    return words;
+}
+
+struct Document {
+    int id;
+    double relevance;
+};
+
+class SearchServer {
+public:
+    void SetStopWords(const string& text) {
+        for (const string& word : SplitIntoWords(text)) {
+            stop_words_.insert(word);
+        }
+    }
+
+    void AddDocument(const int document_id, const string& document) {
+        ++document_count_;
+        const vector<string> words = SplitIntoWordsNoStop(document);
+        const double word_tf = ComputeWordTF(words);
+
+        for (const string& word : words) {
+            documents_[word][document_id] += word_tf;
+        }
+    }
+
+    vector<Document> FindTopDocuments(const string& raw_query) const {
+        const Query query_words = ParseQuery(raw_query);
+        auto matched_documents = FindAllDocuments(query_words);
+
+        sort(matched_documents.begin(), matched_documents.end(),
+            [](const Document& lhs, const Document& rhs) {
+                return lhs.relevance > rhs.relevance;
+            });
+
+        if (matched_documents.size() > MAX_RESULT_DOCUMENT_COUNT) {
+            matched_documents.resize(MAX_RESULT_DOCUMENT_COUNT);
+        }
+        return matched_documents;
+    }
+
+private:
+
+    struct Query {
+        set<string> plus_words;
+        set<string> minus_words;
+    };
+
+    map<string, map<int, double>> documents_;
+    set<string> stop_words_;
+    int document_count_ = 0;
+
+    bool IsStopWord(const string& word) const {
+        return stop_words_.count(word) > 0;
+    }
+
+    bool IsMinusWord(const string& word) const {
+        return word[0] == '-';
+    }
+
+    double ComputeWorfIDF(const string& word) const {
+        return log(static_cast<double>(document_count_) / documents_.at(word).size());
+    }
+
+    double ComputeWordTF(const vector<string>& words) const {
+        return 1. / words.size();
+    }
+
+    vector<string> SplitIntoWordsNoStop(const string& text) const {
+        vector<string> words;
+
+        for (const string& word : SplitIntoWords(text)) {
+            if (!IsStopWord(word)) {
+                words.push_back(word);
+            }
+        }
+        return words;
+    }
+
+    Query ParseQuery(const string& text) const {
+        Query query_words;
+
+        for (const string& word : SplitIntoWordsNoStop(text)) {
+            if (IsMinusWord(word)) {
+                query_words.minus_words.insert(word.substr(1));
+            }
+            else {
+                query_words.plus_words.insert(word);
+            }
+        }
+        return query_words;
+    }
+
+    vector<Document> FindAllDocuments(const Query& query_words) const {
+        vector<Document> matched_documents;
+        map<int, double> relevance;
+
+        for (const auto& word : query_words.plus_words) {
+            if (documents_.count(word)) {
+                const double word_idf = ComputeWorfIDF(word);
+
+                for (const auto& [document_id, word_tf] : documents_.at(word)) {
+                    relevance[document_id] += word_idf * word_tf;
+                }
+            }
+        }
+
+        for (const auto& word : query_words.minus_words) {
+            if (documents_.count(word)) {
+                for (const auto& [document_id, tf_idf] : documents_.at(word)) {
+                    relevance.erase(document_id);
+                }
+            }
+        }
+
+        for (const auto& [document_id, tf_idf] : relevance) {
+            matched_documents.push_back({ document_id, tf_idf });
+        }
+        return matched_documents;
+    }
+};
+
+SearchServer CreateSearchServer() {
+    SearchServer search_server;
+    search_server.SetStopWords(ReadLine());
+    const int document_count = ReadLineWithNumber();
+
+    for (int document_id = 0; document_id < document_count; ++document_id) {
+        search_server.AddDocument(document_id, ReadLine());
+    }
+    return search_server;
+}
+
+int main() {
+    const SearchServer search_server = CreateSearchServer();
+    const string query = ReadLine();
+
+    for (const auto& [document_id, relevance] : search_server.FindTopDocuments(query)) {
+        cout << "{ document_id = "s << document_id << ", "
+            << "relevance = "s << relevance << " }"s << endl;
+    }
+}
